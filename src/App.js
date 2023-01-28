cd8d3bacedf0d28afd64c143af13faa0d90480d1
lala-lee-learning <lala-lee-learning@gmail.com>
2023-01-28 22:41:45 +0800

    簡易購物車：樣式調整

--- src/App.js
+++ src/App.js
@@ -20,10 +20,24 @@ function App() {
               <div className="card">
                 <img src="https://images.unsplash.com/photo-1585653621032-a5fec164ee92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                 <div className="card-body">
-                  <h5 className="card-title">
+                  <h6 className="card-title">
                     <span>全蔬食健康餐</span>
                     <span className="float-end">NT$ 200</span>
-                  </h5>
+                  </h6>
+                  <button type="button" className="btn btn-outline-primary w-100">
+                    加入購物車
+                  </button>
+                </div>
+              </div>              
+            </div>
+            <div className="col">
+              <div className="card">
+                <img src="https://images.unsplash.com/photo-1551106652-a5bcf4b29ab6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c3dlZXRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h6 className="card-title">
+                    <span>全蔬食健康餐</span>
+                    <span className="float-end">NT$ 200</span>
+                  </h6>
                   <button type="button" className="btn btn-outline-primary w-100">
                     加入購物車
                   </button>
@@ -34,10 +48,10 @@ function App() {
               <div className="card">
                 <img src="https://images.unsplash.com/photo-1585653621032-a5fec164ee92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                 <div className="card-body">
-                  <h5 className="card-title">
+                  <h6 className="card-title">
                     <span>全蔬食健康餐</span>
                     <span className="float-end">NT$ 200</span>
-                  </h5>
+                  </h6>
                   <button type="button" className="btn btn-outline-primary w-100">
                     加入購物車
                   </button>
@@ -48,10 +62,10 @@ function App() {
               <div className="card">
                 <img src="https://images.unsplash.com/photo-1585653621032-a5fec164ee92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                 <div className="card-body">
-                  <h5 className="card-title">
+                  <h6 className="card-title">
                     <span>全蔬食健康餐</span>
                     <span className="float-end">NT$ 200</span>
-                  </h5>
+                  </h6>
                   <button type="button" className="btn btn-outline-primary w-100">
                     加入購物車
                   </button>
@@ -62,24 +76,10 @@ function App() {
               <div className="card">
                 <img src="https://images.unsplash.com/photo-1585653621032-a5fec164ee92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                 <div className="card-body">
-                  <h5 className="card-title">
+                  <h6 className="card-title">
                     <span>全蔬食健康餐</span>
                     <span className="float-end">NT$ 200</span>
-                  </h5>
-                  <button type="button" className="btn btn-outline-primary w-100">
-                    加入購物車
-                  </button>
-                </div>
-              </div>              
-            </div>
-            <div className="col">
-              <div className="card">
-                <img src="https://images.unsplash.com/photo-1585653621032-a5fec164ee92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    <span>全蔬食健康餐</span>
-                    <span className="float-end">NT$ 200</span>
-                  </h5>
+                  </h6>
                   <button type="button" className="btn btn-outline-primary w-100">
                     加入購物車
                   </button>
@@ -94,7 +94,12 @@ function App() {
                 <tbody>
                   <tr>
                     <td><a href="#">x</a></td>
-                    <td><img src="" alt="" /></td>
+                    <td>
+                      <img src="https://images.unsplash.com/photo-1585653621032-a5fec164ee92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" 
+                        className="table-image" 
+                        alt="..." 
+                      />
+                    </td>
                     <td>
                       <span>全蔬食健康餐</span>
                       <br/>
