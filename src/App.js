ca1932c495f0c8e867b86ba84fa3b28e46e4b124
lala-lee-learning <lala-lee-learning@gmail.com>
2023-01-29 10:57:28 +0800

    簡易購物車：完成

--- src/App.js
+++ src/App.js
@@ -2,58 +2,13 @@ import { useReducer } from "react";
 import Navbar from "./components/Navbar";
 import Products from "./components/Products";
 import Cart from "./components/Cart";
-import { CartContext } from "./store";
+import { CartContext, cartReducer, cartInit } from "./store";
 
 
 const App = () => {
-  const calculateTotalPrice = (cartList) => {
-    return cartList
-      .map((item) => item.quantity * item.price)
-      .reduce((prev, curr) => prev + curr, 0);
-  };
-  const cartReducer = useReducer((state, action) => {
-    const cartList = [...state.cartList];
-    // #1 先取得當前購物車目標品項的索引
-    const {id, quantity} = action.payload;
-    const index = cartList.findIndex((item) => item.id === id);
-    console.log(index);
-    console.log(action);
-    switch (action.type) {
-      case 'ADD_TO_CART':
-        if(index === -1) {
-          // 未加入購物車
-          cartList.push(action.payload);
-        } else {
-          // 當前購物車的項目與加入的項目一致
-          cartList[index].quantity += quantity;
-        }
-        return {
-          ...state,
-          cartList,
-          total:calculateTotalPrice(cartList),
-        };
-      case 'CHANGE_CART_QUANTITY':
-        cartList[index].quantity = quantity;
-        return {
-          ...state,
-          cartList,
-          total:calculateTotalPrice(cartList),
-        };
-      case 'REMOVE_CART_ITEM':
-        cartList.splice(index, 1);
-        return {
-          ...state,
-          cartList,
-          total:calculateTotalPrice(cartList),
-        };
-      default:
-        return state;
-    }
-  }, {
-    cartList: [],
-  });
+  const reducer = useReducer(cartReducer, cartInit);
   return (
-    <CartContext.Provider value={cartReducer}>
+    <CartContext.Provider value={reducer}>
       <div className="App">
         <Navbar />
         <div className="container mt-4">
--- src/store.js
+++ src/store.js
@@ -1,3 +1,51 @@
 import { createContext } from "react";
 
+const calculateTotalPrice = (cartList) => {
+  return cartList
+    .map((item) => item.quantity * item.price)
+    .reduce((prev, curr) => prev + curr, 0);
+};
+
+export const cartInit = {
+  cartList: [],
+};
+
+export const cartReducer = (state, action) => {
+  const cartList = [...state.cartList];
+  // #1 先取得當前購物車目標品項的索引
+  const {id, quantity} = action.payload;
+  const index = cartList.findIndex((item) => item.id === id);
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      if(index === -1) {
+        // 未加入購物車
+        cartList.push(action.payload);
+      } else {
+        // 當前購物車的項目與加入的項目一致
+        cartList[index].quantity += quantity;
+      }
+      return {
+        ...state,
+        cartList,
+        total:calculateTotalPrice(cartList),
+      };
+    case 'CHANGE_CART_QUANTITY':
+      cartList[index].quantity = quantity;
+      return {
+        ...state,
+        cartList,
+        total:calculateTotalPrice(cartList),
+      };
+    case 'REMOVE_CART_ITEM':
+      cartList.splice(index, 1);
+      return {
+        ...state,
+        cartList,
+        total:calculateTotalPrice(cartList),
+      };
+    default:
+      return state;
+  }
+};
+
 export const CartContext = createContext({});
